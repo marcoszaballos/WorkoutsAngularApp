5995d1ac8082bade3948531691e17138ed6d623b
Marcos <marcoszaballos@gmail.com>
2024-05-10 10:35:17 +0200

    Se elimina dotenv + configuraciones hostings

--- src/environment.ts
+++ src/environment.ts
@@ -1,16 +1,7 @@
-import * as dotenv from 'dotenv';
-
-// Carga las variables de entorno desde el archivo .env
-dotenv.config();
-
-const production = process.env['PRODUCTION'] === 'true';
-const firebaseConfig = {
-  apiKey: process.env['FIREBASE_API_KEY'],
-  authDomain: process.env['FIREBASE_AUTH_DOMAIN']
-};
-
-// Exporta las variables
 export const environment = {
-  production: production,
-  firebaseConfig: firebaseConfig
+  production: false,
+  firebaseConfig: {
+      apiKey: "AIzaSyDqNKLnqUxuXNE156e5tB6KNPghSN5KMH8",
+      authDomain: "app-workouts.firebaseapp.com",
+  }
 };
